1b6a8289c230c7b1f695660cd7f60040068f5e8b
paoloumali <2522851+paoloumali@users.noreply.github.com>
2023-02-25 02:50:13 +0000

    bundle with letter_opener_web instead

--- Gemfile
+++ Gemfile
@@ -66,7 +66,7 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 
-  gem 'letter_opener'
+  gem 'letter_opener_web'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/mail"
+
 
   # Defines the root path route ("/")
   # root "articles#index"
